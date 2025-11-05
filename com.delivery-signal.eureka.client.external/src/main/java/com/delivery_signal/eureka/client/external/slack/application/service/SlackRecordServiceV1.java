bcfee8b0c1effa55c7c3ffb6fa85d18ba111190e
searchcolor <qkrdydwo1022@gmail.com>
2025-11-05 11:19:27 +0900

    docs : package 명 변경

---
+++ com.delivery-signal.eureka.client.external/src/main/java/com/delivery_signal/eureka/client/external/slack/application/service/SlackRecordServiceV1.java
@@ -0,0 +1,26 @@
+package com.delivery_signal.eureka.client.external.slack.application.service;
+
+import com.delivery_signal.eureka.client.external.slack.application.dto.SlackRecordCreationDto;
+import com.delivery_signal.eureka.client.external.slack.application.dto.request.CreateSlackRecordRequest;
+import com.delivery_signal.eureka.client.external.slack.domain.model.SlackRecord;
+import com.delivery_signal.eureka.client.external.slack.domain.repository.SlackRecordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SlackRecordServiceV1 {
+
+    private final SlackRecordRepository repository;
+
+    /**
+     * <p>SlackRecord 생성</p>
+     * @param request 질문한유저 {@link CreateSlackRecordRequest}
+     * @return {@link SlackRecordCreationDto}
+     */
+    public SlackRecordCreationDto createSlackRecord(CreateSlackRecordRequest request){
+        SlackRecord slackRecord = SlackRecord.create(request);
+        repository.save(slackRecord);
+        return SlackRecordCreationDto.from(slackRecord);
+    }
+}
