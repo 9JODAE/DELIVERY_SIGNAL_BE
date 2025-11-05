90df8f3beaf48697ee684724e60ad7aea28d7b41
minjko <komj36@gmail.com>
2025-11-05 14:17:15 +0900

    dummy data 반환값

--- com.delivery-signal.eureka.client.user/src/main/java/com/delivery_signal/eureka/client/user/controller/UserController.java
+++ com.delivery-signal.eureka.client.user/src/main/java/com/delivery_signal/eureka/client/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.delivery_signal.eureka.client.user.controller;
 
 import com.delivery_signal.eureka.client.user.dto.request.UserCreateRequestDto;
 import com.delivery_signal.eureka.client.user.dto.response.UserResponseDto;
+import com.delivery_signal.eureka.client.user.entity.ApprovalStatus;
+import com.delivery_signal.eureka.client.user.entity.UserRole;
 import com.delivery_signal.eureka.client.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,9 +37,22 @@ public class UserController {
 //    @PreAuthorize("hasRole('MASTER')")
 //    @Operation(summary="MASTER의 특정 사용자 조회", description="사용자를 조회합니다")
     public UserResponseDto findUser(@PathVariable("userId") Long userId) {
+        /*
         UserResponseDto responseDto = userService.getUser(userId);
 
         return responseDto;
+
+        */
+
+        // dummy data
+        return new UserResponseDto(
+                2L,
+                "kimhubgwan",
+                "UKIMHUBGWAN",
+                "서울특별시 센터 허브",
+                UserRole.HUB_MANAGER,
+                ApprovalStatus.APPROVED
+        );
     }
 
     @GetMapping()
@@ -46,9 +61,78 @@ public class UserController {
     public List<UserResponseDto> searchUser(
 //            @Parameter(description = "사용자 이름, 사용자 소속 그룹(허브 또는 업체)명, 권한으로 검색 가능")
             @RequestParam(required = false) String search) {
+        /*
         List<UserResponseDto> responseDtos = userService.getUsers(search);
 
         return responseDtos;
+
+        */
+
+        // dummy data
+        return List.of(
+                new UserResponseDto(1L, "choigoim", "UCHOIGOIM", "delivery-signal", UserRole.MASTER, ApprovalStatus.PENDING),
+
+                // 허브 관리자
+                new UserResponseDto(2L, "kimhubgwan", "UKIMHUBGWAN", "서울특별시 센터 허브", UserRole.HUB_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(3L, "leehubgwan", "ULEEHUBGWAN", "세종특별자치시 센터 허브", UserRole.HUB_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(4L, "sohubgwan", "USOHUBGWAN", "경기 남부 센터 허브", UserRole.HUB_MANAGER, ApprovalStatus.PENDING),
+
+                // 배송 관리자 (허브)
+                new UserResponseDto(5L, "jeonbaehub", "UJEONBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(6L, "yubaehub", "UYUBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(7L, "minbaehub", "UMINBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(8L, "kobaehub", "UKOBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(9L, "kimbaehub", "UKIMBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(10L, "parkbaehub", "UPARKBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(11L, "jinbaehub", "UJINBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(12L, "yangbaehub", "UYANGBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(13L, "rhabaehub", "URHABAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(14L, "woobaehub", "UWOOBAEHUB", "delivery-signal", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+
+                // 업체 배송 관리자 - 서울특별시 센터 허브
+                new UserResponseDto(15L, "nabaecom", "UNABAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(16L, "limbaecom", "ULIMBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(17L, "jungbaecom", "UJUNGBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(18L, "hanbaecom", "UHANBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(19L, "ohbaecom", "UOHBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(20L, "seobaecom", "USEOBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(21L, "hwangbaecom", "UHWANGBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(22L, "ahnbaecom", "UAHNBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(23L, "songbaecom", "USONGBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(24L, "hongbaecom", "UHONGBAECOM", "서울특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+
+                // 업체 배송 관리자 - 세종특별자치시 센터 허브
+                new UserResponseDto(25L, "simbaecom", "USIMBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(26L, "nobaecom", "UNOBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(27L, "habaecom", "UHABAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(28L, "sungbaecom", "USUNGBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(29L, "chabaecom", "UCHABAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(30L, "joobaecom", "UJOOBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(31L, "munbaecom", "UMUNBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(32L, "sonbaecom", "USONBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(33L, "yeonbaecom", "UYEONBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(34L, "bangbaecom", "UBANGBAECOM", "세종특별자치시 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+
+                // 업체 배송 관리자 - 경기 남부 센터 허브
+                new UserResponseDto(35L, "jibaecom", "UJIBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(36L, "kibaecom", "UKIBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(37L, "pyobaecom", "UPYOBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(38L, "gubaecom", "UGUBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(39L, "myeongbaecom", "UMYEONGBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(40L, "wangbaecom", "UWANGBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(41L, "dobaecom", "UDOBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(42L, "mabaecom", "UMABAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(43L, "jebaecom", "UJEBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(44L, "mobaecom", "UMOBAECOM", "경기 남부 센터 허브", UserRole.DELIVERY_MANAGER, ApprovalStatus.PENDING),
+
+                // 업체 관리자
+                new UserResponseDto(45L, "yooncomgwan", "UYOONCOMGWAN", "A 업체", UserRole.SUPPLIER_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(46L, "gilcomgwan", "UGILCOMGWAN", "B 업체", UserRole.SUPPLIER_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(47L, "euncomgwan", "UEUNCOMGWAN", "C 업체", UserRole.SUPPLIER_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(48L, "okgwan", "UOKCOMGWAN", "D 업체", UserRole.SUPPLIER_MANAGER, ApprovalStatus.PENDING),
+                new UserResponseDto(49L, "takgwan", "UTAKCOMGWAN", "E 업체", UserRole.SUPPLIER_MANAGER, ApprovalStatus.PENDING)
+
+        );
     }
 
 
