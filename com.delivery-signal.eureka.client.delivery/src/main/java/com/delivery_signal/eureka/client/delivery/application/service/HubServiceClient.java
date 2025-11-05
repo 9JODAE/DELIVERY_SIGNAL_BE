e0942576013cef06db1073caafdfda2584c85811
songkyung <min.songkyung@gmail.com>
2025-11-05 13:42:30 +0900

    feature: HubServiceClient 임시 생성

--- com.delivery-signal.eureka.client.delivery/src/main/java/com/delivery_signal/eureka/client/delivery/application/service/HubServiceClient.java
+++ com.delivery-signal.eureka.client.delivery/src/main/java/com/delivery_signal/eureka/client/delivery/application/service/HubServiceClient.java
@@ -4,4 +4,23 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name = "hub-service", path = "/v1/hubs")
 public interface HubServiceClient {
+
+    /**
+     * 허브 이동 정보 목록 조회 (존재 여부 확인 및 정보 획득)
+     * 요구사항: 주문 요청 들어올 시, 배송과 배송 경로 기록이 생성되어야 함
+     * (배송 경로 기록 : 배송 경로는 최초에 모든 경로가 생성되어야 함)
+     */
+
+    // TODO: 추후 수정 예정
+//    @GetMapping("/api/hub-routes/{order-id}")
+//    List<RouteSegmentResponse> getHubRoutes(@PathVariable Long orderId);
+
+    // (HubRoute Service의 경로 계산 API 명세 기반)
+//    public record RouteSegmentResponse(
+//        Integer sequence,
+//        Long departureHubId,
+//        Long arrivalHubId,
+//        Double expectedDistance, // 예상 거리
+//        Long expectedDuration // 예상 소요 시간 (초)
+//    ) {}
 }
--- com.delivery-signal.eureka.client.delivery/src/main/java/com/delivery_signal/eureka/client/delivery/presentation/controller/DeliveryController.java
+++ com.delivery-signal.eureka.client.delivery/src/main/java/com/delivery_signal/eureka/client/delivery/presentation/controller/DeliveryController.java
@@ -6,8 +6,10 @@ import com.delivery_signal.eureka.client.delivery.presentation.dto.request.Deliv
 import com.delivery_signal.eureka.client.delivery.presentation.dto.request.DeliveryManagerRegisterRequest;
 import com.delivery_signal.eureka.client.delivery.presentation.dto.response.DeliveryCreateResponse;
 import jakarta.validation.Valid;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,6 +32,12 @@ public class DeliveryController {
         this.deliveryService = deliveryService;
     }
 
+    // TEST 엔드포인트
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status(HttpStatus.OK).body("Delivery Service 통신 성공");
+    }
+
     @PostMapping
     public ResponseEntity<DeliveryCreateResponse> createDelivery(
         @Valid @RequestBody DeliveryCreateRequest request,
@@ -40,25 +48,3 @@ public class DeliveryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
-
-//FeignClient 인터페이스 정의: OrderService, HubRouteService 등 다른 서비스의 API를 호출하기 위한 FeignClient를 정의합니다.
-
-//
-
-//DTO 정의: 주문 생성 요청을 받아 배송 엔티티를 생성하고, 다른 서비스에 데이터를 요청할 때 사용할 DTO를 정의합니다.
-
-//
-
-//서비스 로직 구현:
-
-//
-
-//트랜잭션 관리: @Transactional을 사용하여 주문 생성, 배송 엔티티 생성, 재고 감소 등이 하나의 논리적 단위로 묶이도록 합니다. 실패 시 롤백.
-
-//
-
-//서비스 간 통신: FeignClient를 사용하여 주문 정보, 허브 경로 정보를 가져옵니다.
-
-//
-
-//배송 경로 생성: 주문 정보와 허브 경로 정보를 바탕으로 전체 배송 경로 (DeliveryRouteLog)를 최초에 모두 생성합니다. (경로 모델 선택에 따라 로직 복잡도 달라짐. 여기서는 P2P를 가정)
