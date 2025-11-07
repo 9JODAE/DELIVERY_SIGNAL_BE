9395b8970681ddf78e1f4d89485c7f79a1e6515a
JIUNG YANG <yangjiung286@gmail.com>
2025-11-08 01:13:20 +0900

    feat(hub-stock): ProductId VO 추가

---
+++ com.delivery-signal.eureka.client.hub/src/main/java/com/delivery_signal/eureka/client/hub/domain/vo/ProductId.java
@@ -0,0 +1,29 @@
+package com.delivery_signal.eureka.client.hub.domain.vo;
+
+import java.util.UUID;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductId {
+
+	private UUID productId;
+
+	public ProductId(UUID productId) {
+		if (productId == null) {
+			throw new IllegalArgumentException("유효하지 않은 상품 ID입니다");
+		}
+		this.productId = productId;
+	}
+
+	public static  ProductId of(UUID productId) {
+		return new ProductId(productId);
+	}
+}
