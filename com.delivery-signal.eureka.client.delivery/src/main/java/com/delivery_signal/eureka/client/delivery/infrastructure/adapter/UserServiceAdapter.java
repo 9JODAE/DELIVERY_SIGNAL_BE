51fe4d98768c4f2ddbf1da5d90fc4faf3562f8d0
songkyung <min.songkyung@gmail.com>
2025-11-12 09:01:41 +0900

    refactor: user service client 연동 및 Validator 클래스 생성하여 권한 판별 로직 위임

--- com.delivery-signal.eureka.client.delivery/src/main/java/com/delivery_signal/eureka/client/delivery/infrastructure/adapter/UserServiceAdapter.java
+++ com.delivery-signal.eureka.client.delivery/src/main/java/com/delivery_signal/eureka/client/delivery/infrastructure/adapter/UserServiceAdapter.java
@@ -1,5 +1,6 @@
 package com.delivery_signal.eureka.client.delivery.infrastructure.adapter;
 
+import com.delivery_signal.eureka.client.delivery.application.port.UserAuthPort;
 import com.delivery_signal.eureka.client.delivery.application.service.UserServiceClient;
 import com.delivery_signal.eureka.client.delivery.domain.vo.AuthorizedUser;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
  * ACL: 외부 User 서비스로부터 사용자 인증/활성 정보를 조회하는 어댑터
  */
 @Component
-public class UserServiceAdapter {
+public class UserServiceAdapter implements UserAuthPort {
     private final UserServiceClient userServiceClient;
 
     public UserServiceAdapter(UserServiceClient userServiceClient) {
@@ -18,6 +19,7 @@ public class UserServiceAdapter {
     /**
      * User Service로부터 인가에 필요한 상세 정보를 조회하여 Domain VO로 변환
      */
+    @Override
     public AuthorizedUser fetchUserAuthorizationInfo(Long userId) {
         // FeignClient를 통해 User Service 호출 (인프라 상세 구현)
         UserServiceClient.AuthUserResponseDto response =
