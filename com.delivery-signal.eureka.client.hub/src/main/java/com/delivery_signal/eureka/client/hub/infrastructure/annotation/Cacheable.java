a161be459be2c625d0687207ac45a82c9cb90605
JIUNG YANG <yangjiung286@gmail.com>
2025-11-12 11:31:35 +0900

    feat(redis): Cacheable, CacheEvict Annotation 및 각 Aspect 구현

---
+++ com.delivery-signal.eureka.client.hub/src/main/java/com/delivery_signal/eureka/client/hub/infrastructure/annotation/Cacheable.java
@@ -0,0 +1,16 @@
+package com.delivery_signal.eureka.client.hub.infrastructure.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Cacheable {
+	String value();
+	String key();
+	long ttl() default 1;
+}
