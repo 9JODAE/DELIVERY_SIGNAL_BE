405c63202b34b14a43be01341b0cb3a070c2236f
JIUNG YANG <yangjiung286@gmail.com>
2025-11-12 21:40:43 +0900

    feat(hub): JpaAuditConfig 추가

--- com.delivery-signal.eureka.client.hub/src/main/java/com/delivery_signal/eureka/client/hub/infrastructure/config/JpaAuditConfig.java
+++ com.delivery-signal.eureka.client.hub/src/main/java/com/delivery_signal/eureka/client/hub/infrastructure/config/JpaAuditConfig.java
@@ -1,15 +1,28 @@
 package com.delivery_signal.eureka.client.hub.infrastructure.config;
 
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.delivery_signal.eureka.client.hub.common.auth.UserContextHolder;
+import com.delivery_signal.eureka.client.hub.common.error.HubErrorCode;
+import com.delivery_signal.eureka.client.hub.common.exception.UnauthorizedException;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditConfig {
 
-	// @Bean TODO 추후 유저 서비스 이후 개발
-	// public AuditorAware<Long> auditorProvider() {
-	// 	// 현재 로그인한 사용자의 이름 반환
-	//
-	// }
+	@Bean
+	public AuditorAware<Long> auditorProvider() {
+		return () -> {
+			String userId = UserContextHolder.getUserId();
+			if (userId == null || userId.isBlank()) {
+				throw new UnauthorizedException(HubErrorCode.UNAUTHORIZED);
+			}
+			return Optional.of(Long.parseLong(userId));
+		};
+	}
 }
